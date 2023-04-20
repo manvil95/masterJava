ac4b11e31dc616417b4ca9d53cc15f5bfa401265
Manuel Villén <manuvillen@gmail.com>
2023-04-20 12:09:43 +0200

    Unificando

---
+++ helloWorld/HolaMundo/src/OperadorAritmetico.java
@@ -0,0 +1,8 @@
+public class OperadorAritmetico {
+    public static void main(String[] args) {
+
+        int i = 5, j = 4, suma = i + j, resta = i - j, multiplicacion = i * j;
+
+        System.out.println("suma = " + suma);
+    }
+}
